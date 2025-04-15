d59b7f7f4c09c41d4569f0e39ac8197f7291fd35
[sushmakurraSDET] <skurra14@gmail.com>
2025-04-15 01:06:24 -0400

    Added home,reg and graph

---
+++ src/test/java/StepDefinitions/GraphPageStep.java
@@ -0,0 +1,178 @@
+package StepDefinitions;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GraphPageStep {
+
+	@Given("The user is on the Home page")
+	public void the_user_is_on_the_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+	@When("The user selects Graph frpm Data Structures")
+	public void the_user_selects_graph_frpm_data_structures() {
+	    // Write code here that turns the phrase above into concrete actions
+	 
+	}
+
+	@Then("The user should able to see an warning message {string}")
+	public void the_user_should_able_to_see_an_warning_message(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("The user clicks Get Started buttons of Graph on the homepage without Sign in")
+	public void the_user_clicks_get_started_buttons_of_graph_on_the_homepage_without_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("The user should able to see an warning message_graph {string}")
+	public void the_user_should_able_to_see_an_warning_message_graph(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("The user gets data from excel sheet {string} and {int} for the login page and clicks Graphs Get Started button")
+	public void the_user_gets_data_from_excel_sheet_and_for_the_login_page_and_clicks_graphs_get_started_button(String string, Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+	@Then("The user should able to redirect to the Graph page")
+	public void the_user_should_able_to_redirect_to_the_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("The user is on the Graph page")
+	public void the_user_is_on_the_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("The user clicks on the sublink Graph on the Graph page")
+	public void the_user_clicks_on_the_sublink_graph_on_the_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("The user should redirect to the sublink Graph page")
+	public void the_user_should_redirect_to_the_sublink_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("The user is on the sublink Graph page")
+	public void the_user_is_on_the_sublink_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("The user clicks on the button Try here >>> from the sublink Graph page")
+	public void the_user_clicks_on_the_button_try_here_from_the_sublink_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("The user should able to redirect to a new page with text area for trying the code")
+	public void the_user_should_able_to_redirect_to_a_new_page_with_text_area_for_trying_the_code() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("The user is on the try here textarea of sublink Graph page")
+	public void the_user_is_on_the_try_here_textarea_of_sublink_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("The user gets python code from excel sheet {string} and {int} for the tryeditor and click run button")
+	public void the_user_gets_python_code_from_excel_sheet_and_for_the_tryeditor_and_click_run_button(String string, Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("The user should able to get the result from excel sheet {string} and {int}")
+	public void the_user_should_able_to_get_the_result_from_excel_sheet_and(String string, Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("I enter the code from excel sheet {string} and {int}")
+	public void i_enter_the_code_from_excel_sheet_and(String string, Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("The user should be able to receive an alert message from excel sheet {string} and {int}")
+	public void the_user_should_be_able_to_receive_an_alert_message_from_excel_sheet_and(String string, Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("The user try to click on the browser back button")
+	public void the_user_try_to_click_on_the_browser_back_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("The user should be able to navigate to the previous page from the try here page ie, Graph page")
+	public void the_user_should_be_able_to_navigate_to_the_previous_page_from_the_try_here_page_ie_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("The user clicks on the link Graph Representations on the Graph page")
+	public void the_user_clicks_on_the_link_graph_representations_on_the_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("The user should redirect to the Graph Representations page")
+	public void the_user_should_redirect_to_the_graph_representations_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("The user is on the Graph Representations page")
+	public void the_user_is_on_the_graph_representations_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("The user clicks on the button Try here >>> from the Graph Representations page")
+	public void the_user_clicks_on_the_button_try_here_from_the_graph_representations_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Given("The user is on the try here textarea of Graph Representations page")
+	public void the_user_is_on_the_try_here_textarea_of_graph_representations_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("The user should be able to navigate to the previous page from the try here page ie, Graph Representations page")
+	public void the_user_should_be_able_to_navigate_to_the_previous_page_from_the_try_here_page_ie_graph_representations_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("The user clicks on the link Practice Questions on the Graph page")
+	public void the_user_clicks_on_the_link_practice_questions_on_the_graph_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("The user should redirect to the Practice Questions page")
+	public void the_user_should_redirect_to_the_practice_questions_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+
+
+}
